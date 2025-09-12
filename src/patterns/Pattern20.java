76c1677a790ad00f34f35564a4ff5a73fc2f2a91
himanshup-work <himanshu.work2703@gmail.com>
2025-09-12 22:45:56 +0530

    solved 3 pattern programs

--- src/patterns/Pattern20.java
+++ src/patterns/Pattern20.java
@@ -1,14 +1,14 @@
 package patterns;
 
-public class Pattern19 {
+public class Pattern20 {
     public static void main(String[] args) {
         int n =5;
-        for (int i = 1; i <= 2*n; i++) {
-            int stars = n-i+1;
-            int spaces = 2*(i-1);
+        for (int i = 1; i < 2*n; i++) {
+            int stars = i;
+            int spaces = 2*(n-i);
             if (i>n){
-                stars = i-n;
-                spaces = 2*(2*n-i);
+                stars = 2*n-i;
+                spaces = 2*(i-n);
             }
 
             for (int j = 1; j <= stars; j++) {
