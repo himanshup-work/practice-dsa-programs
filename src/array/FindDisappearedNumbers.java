4739ed4b2420cf6281b144680cc3c1c5ae9d3c20
himanshu-inexture <himanshup.inexture@gmail.com>
2025-10-09 18:37:26 +0530

    fixed the FindDisappearedNumbers program

--- src/array/FindDisappearedNumbers.java
+++ src/array/FindDisappearedNumbers.java
@@ -4,26 +4,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class LongestCommonPrefix {
+public class FindDisappearedNumbers {
     public static void main(String[] args) {
-        LongestCommonPrefix l = new LongestCommonPrefix();
+        FindDisappearedNumbers l = new FindDisappearedNumbers();
         int[] arr = {1,2,3,3,4,5,6,6,7,8};
         System.out.println(l.findDisappearedNumbers(arr));
     }
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int n = nums.length;
+        int i = 1;
         int j = 0;
         Arrays.sort(nums);
         List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            if (j > 0 && nums[j] == nums[j-1]){
+        while (i<=n){
+            if (j<n && j > 0 && nums[j] == nums[j-1]){
                 j++;
+                continue;
             }
-            if (nums[j] == i){
-                list.add(i);
+            if (j < n && nums[j] == i){
+                i++;
                 j++;
+            } else {
+                list.add(i);
+                i++;
             }
         }
+
         return list;
     }
 }
